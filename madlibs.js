f9da701015c26687c0387677776ffe289c349eac
Guillermo Cabral <guille.cabral87@gmail.com>
2021-07-21 10:30:52 -0400

    leave some message

--- madlibs.js
+++ madlibs.js
@@ -9,15 +9,15 @@ function writeStory() {
     var name7 = document.getElementById("name7").value;
     var noun8 = document.getElementById("noun8").value;
     var liq9 = document.getElementById("liq9").value;
-    var verb10 = document.getElementByIdr("verb10").value;
+    var verb10 = document.getElementById("verb10").value;
     var part11 = document.getElementById("part11").value;
     var noun12 = document.getElementById("noun12").value;
     var verb13 = document.getElementById("verb13").value;
     var noun14 = document.getElementById("noun14").value;
 
 
-    var paragraph = "<p>It was a " + adj1.value + ", cold November day. I woke up to the " + adj2.value + " smell of " + bird3.value + " roasting in the " + house4.value + " downstairs. I "+ verb5.value + " down the stairs to see if I could help " + verb6 + " the dinner. My mom said, \"See if " + name7.value + " needs a fresh " + noun8.value + "\. So I carried a tray of glasses full of " + liq9.value + " into the " + verb10.value + "room. When I got there, I couldn't believe my " + part11.value + "! There were " + noun12.value + " " + verb13.value + " on the " + noun14.value + "!.</p>"
+    var paragraph = "<p>It was a " + adj1 + ", November day. I woke up to the " + adj2 + " smell of " + bird3 + " roasting in the " + house4 + " downstairs. I "+ verb5 + " down the stairs to see if I could help " + verb6 + " the dinner. My mom said, \"See if " + name7 + " needs a fresh " + noun8 + ".\" So I carried a tray of glasses full of " + liq9 + " into the " + verb10 + "room. When I got there, I couldn't believe my " + part11 + "! There were " + noun12 + " " + verb13 + " on the " + noun14 + "!.</p>"
 
 
-    document.getElementById("answer").innerHTML = paragraph;
+    document.getElementById("outputPanel").innerHTML = paragraph;
 }
